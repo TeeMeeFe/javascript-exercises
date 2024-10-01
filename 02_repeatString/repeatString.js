898f3032d3404d36cbc11eef01f3280718289c89
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-01 14:13:26 -0300

    02: Add functionality for the first test, it now GREENS

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeatTimes = 1) {
+    let str = '';
 
+    for( let i = 0; i < repeatTimes; i++) {
+        str += string;
+    }
+
+    return str;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
