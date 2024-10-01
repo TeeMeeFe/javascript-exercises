3dda7c83b6208eaa022c234e30584a694942700a
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-01 18:44:16 -0300

    04: Finish removeFromArray(); Unskip a test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // Create a brand spanking new array
+    const newArray = [];
+ 
+    // Search for each element existing in the old array and negate its inclusion when pushing to the new array
+    arr.forEach((item) => {
+        if( !args.includes(item, 0) ) {
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
 };
+
+const result = removeFromArray([1, 2, 3, 4], 1, 2);
+console.log(result);
 
 // Do not edit below this line
 module.exports = removeFromArray;
