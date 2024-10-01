01eb5a56e95421e60804dc5fbafc4e3ed911463c
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-01 15:42:20 -0300

    02: Expand repeatString() to cover all test cases; Unskip the rest of the test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
 const repeatString = function(string, repeatTimes = 1) {
     let str = '';
 
+    if( repeatTimes < 0 ) { return "ERROR";}
+
     for( let i = 0; i < repeatTimes; i++) {
         str += string;
     }
@@ -9,6 +11,12 @@ const repeatString = function(string, repeatTimes = 1) {
 };
 
 repeatString('hey', 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye',0);
+repeatString('goodbye',-1);
+repeatString('odin', 14);
+repeatString('', 4);
 
 // Do not edit below this line
 module.exports = repeatString;
