b7979065bc2062f402da1d849ac21bea8d77a277
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-01 16:33:20 -0300

    03: Finish reverseString() function; First test GREENS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // Split the string into several substrings
+    const splitString = str.split("");
+    // Reverse these strings 
+    const reversedString = splitString.reverse();
+    // Join them back together into a single string again
+    const joinedString = reversedString.join("");
 
+    return joinedString;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
