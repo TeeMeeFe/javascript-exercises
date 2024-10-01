3697e38de75f2c10bc07db7fd66518c719c29e62
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-01 20:22:29 -0300

    05: Finish sumAll function, first test GREEN

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startIndex, endIndex) {
+    const start = parseInt(Math.min(startIndex, endIndex));
+    const end = parseInt(Math.max(startIndex, endIndex));
+    let i = start;
+    let num = start;
 
+    if( start < 0 || end < 0 || typeof start !== "number" || typeof end !== "number" ) { return 'ERROR' };
+
+    do { 
+        num += start + i;
+        i++; 
+    } 
+    while( i < end );
+
+    return num;
 };
+
+const result = sumAll(1, 4);
+console.log(result);
 
 // Do not edit below this line
 module.exports = sumAll;
