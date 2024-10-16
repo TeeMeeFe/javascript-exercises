0742fa8d541bd1f4932bda5bc3556b0010086f78
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-16 20:40:00 -0300

    10: Finish the logic for the fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let f_0 = 0;
+    let f_1 = 1;
+    let sum;
+    if( a < 0 ) { return "OOPS" }
+    else if( a == 0 ) { return 0 }
+    else if( a == 1 || a == 2 ) { return 1 }
+    else {
+        for( let i = 2; i <= a; i++ ){
+            sum = f_0 + f_1;
+            f_0 = f_1;
+            f_1 = sum;
+        }
+    }
+    return sum;
+};  
 
-};
+console.log(fibonacci(0));
 
 // Do not edit below this line
 module.exports = fibonacci;
