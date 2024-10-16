f30cfda5507ac823abd28d1fd77d9406c6bf987f
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-16 19:47:19 -0300

    09: Expand logic a bit more but still fails in some tests
    I spoiled myself when i saw the solution, so i wont be fixing this

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,19 @@
 const palindromes = function (str) {
-    const original = (string) => string.split("").join();
-    const reversed = (string) => string.split("").reverse().join();
-    for( let i = 0; i < str.length; i++) {
+    const original = (string) => string.toLowerCase().split(", ").join();
+    const reversed = (string) => string.toLowerCase().split(", ").reverse().join();
+    for( let i = 1; i <= str.length; i++) {
         let spliced = str.substr(0, i) + str.substr(i + 1);
         if( original(spliced) === reversed(spliced) ) { return true };
     }
     return false;
 };
 
+// Debug stuff!
 const stringy = "Animal loots foliated detail of stool lamina."
 
 console.log(palindromes(stringy));
-console.log(stringy.split("").join());
-console.log(stringy.split("").reverse().join());
+console.log(stringy.toLowerCase().split(", ").join());
+console.log(stringy.toLowerCase().split(", ").reverse().join());
 
 // Do not edit below this line
 module.exports = palindromes;
