e8e814f1b3772a4671448a4d55acc4f2b7ccc053
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-16 18:17:22 -0300

    09: Unskip the rest of tests but some of them DON'T PASS
    Add some debug console.logs

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,18 @@
 const palindromes = function (str) {
-    const original = (string) => string.split(" ").join();
-    const reversed = (string) => string.split(" ").reverse().join();
+    const original = (string) => string.split("").join();
+    const reversed = (string) => string.split("").reverse().join();
     for( let i = 0; i < str.length; i++) {
-        let spliced = str.substr(0, i) + str.substr(i+1);
+        let spliced = str.substr(0, i) + str.substr(i + 1);
         if( original(spliced) === reversed(spliced) ) { return true };
     }
     return false;
 };
+
+const stringy = "Animal loots foliated detail of stool lamina."
+
+console.log(palindromes(stringy));
+console.log(stringy.split("").join());
+console.log(stringy.split("").reverse().join());
 
 // Do not edit below this line
 module.exports = palindromes;
