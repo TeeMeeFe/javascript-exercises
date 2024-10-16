68b5e0cd03fade0e3782f19a6e69a0f1439bd25e
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-16 18:09:03 -0300

    09: Add logic to the palindromes function; First test PASS

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const original = (string) => string.split(" ").join();
+    const reversed = (string) => string.split(" ").reverse().join();
+    for( let i = 0; i < str.length; i++) {
+        let spliced = str.substr(0, i) + str.substr(i+1);
+        if( original(spliced) === reversed(spliced) ) { return true };
+    }
+    return false;
 };
 
 // Do not edit below this line
