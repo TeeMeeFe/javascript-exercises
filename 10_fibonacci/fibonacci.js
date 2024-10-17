85643205e90a8805dc220889946116f59fab39fc
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-16 21:26:36 -0300

    10: Some tweaks and add an extra test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -2,11 +2,12 @@ const fibonacci = function(a) {
     let f_0 = 0;
     let f_1 = 1;
     let sum;
-    if( a < 0 ) { return "OOPS" }
-    else if( a == 0 ) { return 0 }
-    else if( a == 1 || a == 2 ) { return 1 }
+    const input = parseInt(a);
+    if( input < 0 || isNaN(input) ) { return "OOPS" }
+    else if( input == 0 ) { return 0 }
+    else if( input == 1 || input == 2 ) { return 1 }
     else {
-        for( let i = 2; i <= a; i++ ){
+        for( let i = 2; i <= input; i++ ) {
             sum = f_0 + f_1;
             f_0 = f_1;
             f_1 = sum;
@@ -14,8 +15,6 @@ const fibonacci = function(a) {
     }
     return sum;
 };  
-
-console.log(fibonacci(0));
 
 // Do not edit below this line
 module.exports = fibonacci;
