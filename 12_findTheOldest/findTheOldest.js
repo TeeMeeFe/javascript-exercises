edd4770257c66f07f559c92b68a04a9f36ff3c6b
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-18 20:45:23 -0300

    12: Finish the logic for the function: first test PASS

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const today = Date.now();
+    return arr.reduce((prevPerson, currPerson) => {
+        let prevAge = (prevPerson.yearOfDeath - prevPerson.yearOfBirth)
+        let currAge = (currPerson.yearOfDeath - currPerson.yearOfBirth)
+        return prevAge > currAge ? prevPerson : currPerson
+    });
 };
+
+// Debug object, remove before flight!
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    }
+  ]
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
