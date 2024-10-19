d8e0f6bef67393b12625b3e2b7ff17c97d63d373
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2024-10-19 11:40:12 -0300

    12: Nullish coalescense ftw!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,8 @@
 const findTheOldest = function(arr) {
     const today = new Date().getFullYear();
     return arr.reduce((prevPerson, currPerson) => {
-        let prevAge = (prevPerson.yearOfDeath !== undefined ? prevPerson.yearOfDeath : today) - prevPerson.yearOfBirth
-        let currAge = (currPerson.yearOfDeath !== undefined ? currPerson.yearOfDeath : today) - currPerson.yearOfBirth
+        let prevAge = (prevPerson.yearOfDeath ?? today) - prevPerson.yearOfBirth
+        let currAge = (currPerson.yearOfDeath ?? today) - currPerson.yearOfBirth
         return prevAge > currAge ? prevPerson : currPerson
     });
 };
